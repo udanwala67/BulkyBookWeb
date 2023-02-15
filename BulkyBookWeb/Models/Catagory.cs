bcf6f892211886b290cde85be263edf4336096ea
pct62 <pct62@PCT62>
2023-02-15 13:50:08 +0530

    Add project files.

---
+++ BulkyBookWeb/Models/Catagory.cs
@@ -0,0 +1,10 @@
+﻿namespace BulkyBookWeb.Models
+{
+    public class Catagory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DispalyOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
